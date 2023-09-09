ed20a4f046bfc1824ec16f36010823bec3330322
Hamz <h4mz1221@gmail.com>
2023-09-09 02:08:34 +0100

    Track which tab is currently active with different border colour on click

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -2,16 +2,21 @@
 
 import Image from 'next/image'
 import Link from 'next/link'
+import { useState } from 'react';
 import { scrollIntoView } from "seamless-scroll-polyfill";
 
-const scrollToSection = (sectionId) => {
-  const section = document.querySelector(sectionId);
-  if (section) {
-      scrollIntoView(section, {behavior: 'smooth'})
-  }
-};
 
 const Nav = () => {
+    const [activeTab, setActiveTab] = useState('#home')
+
+    const scrollToSection = (sectionId) => {
+    const section = document.querySelector(sectionId);
+        if (section) {
+        setActiveTab(sectionId)
+        scrollIntoView(section, {behavior: 'smooth'})
+    }
+    };
+
     return (
       <nav className='flex-between w-full pt-3 fixed md:px-10 px-3 top-0 backdrop-blur bg-white/20 shadow-lg'>
           <Link href="/" className='flex gap-2 flex-center'>
@@ -29,31 +34,31 @@ const Nav = () => {
           <div className='sm:flex hidden'>
               <div className='flex gap-3'>
                   <Link href='#home'
-                        className='black_btn'
+                        className={`black_btn ${activeTab === '#home' && 'active'}`}
                         scroll={false}
                     onClick={() => scrollToSection('#home')}>
                       Home
                   </Link>
                   <Link href='#about'
-                        className='black_btn'
+                        className={`black_btn ${activeTab === '#about' && 'active'}`}
                         scroll={false}
                     onClick={() => scrollToSection('#about')}>
                       About
                   </Link>
                   <Link href='#events'
-                        className='black_btn'
+                        className={`black_btn ${activeTab === '#events' && 'active'}`}
                         scroll={false}
                     onClick={() => scrollToSection('#events')}>
                       Events
                     </Link>
                     <Link href='#contact'
-                        className='black_btn'
+                        className={`black_btn ${activeTab === '#contact' && 'active'}`}
                         scroll={false}
                     onClick={() => scrollToSection('#contact')}>
                       Contact
                     </Link>
                     <Link href='#sponsors'
-                        className='black_btn'
+                        className={`black_btn ${activeTab === '#sponsors' && 'active'}`}
                         scroll={false}
                     onClick={() => scrollToSection('#sponsors')}>
                       Sponsors
