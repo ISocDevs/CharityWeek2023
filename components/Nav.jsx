95babd2cafd816e8a88576697dee57ece0730387
Hamz <h4mz1221@gmail.com>
2023-09-11 11:31:12 +0100

    Add donate button to nav bar

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -4,14 +4,16 @@ import Image from 'next/image'
 import Link from 'next/link'
 import { createContext, useContext, useEffect, useState } from 'react';
 import { scrollIntoView } from "seamless-scroll-polyfill";
+import DonateButton from './DonateButton';
 
 const NavbarContext = createContext()
 
 
 const Nav = () => {
-    const [activeTab, setActiveTab] = useState('#home')
+    const [activeTab, setActiveTab] = useState('')
+    const [toggleDropdown, setToggleDropdown] = useState(false);
     const [clientWindowHeight, setClientWindowHeight] = useState("")
-    const tabs = ['Home', 'About', 'Events', 'Contact', 'Sponsors']
+    const tabs = ['Home', 'About', 'Events', 'Contact', 'Sponsors', 'Socials']
     
     const scrollToSection = (sectionId) => {
         const section = document.querySelector(sectionId);
@@ -24,6 +26,10 @@ const Nav = () => {
     const handleScroll = () => {
         setClientWindowHeight(window.scrollY);
     };
+
+    useEffect(() => {
+        console.log(toggleDropdown)
+    },[toggleDropdown])
 
     useEffect(() => {
     window.addEventListener("scroll", handleScroll);
@@ -40,7 +46,8 @@ const Nav = () => {
     },[clientWindowHeight])
 
     return (
-      <nav className='flex-between w-full pt-3 fixed md:px-10 px-3 top-0 backdrop-blur bg-white/20 shadow-lg'>
+        <nav className='flex-between w-full py-2 fixed lg:px-10 px-2 top-0 backdrop-blur bg-white/20 shadow-lg'>
+            {/* Logo */}
           <Link href="/" className='flex gap-2 flex-center'>
               <Image
                   src="/assets/images/cw-logo-orange.ico"
@@ -53,14 +60,29 @@ const Nav = () => {
           </Link>
 
           {/* Desktop Navigation */}
-          <div className='sm:flex hidden'>
-                <div className='flex gap-3'>
+          <div className='md:flex hidden justify-center'>
+                <div className='flex lg:gap-3 gap-1'>
                     <NavbarContext.Provider value={activeTab}>
                         {tabs.map(tab =>
                             <Tab name={tab} scrollToSection={scrollToSection} key={tab} />)}
                     </NavbarContext.Provider>
+                    <DonateButton style={'orange'}/>
               </div>
-          </div>
+            </div>
+            
+            {/* Mobile navigation */}
+            <div className='md:hidden flex-center w-full absolute'>
+                <DonateButton style={'pulse large'}/>
+                </div>
+            <div className='md:hidden'>
+                <div className=''>
+                    <Image
+                        src='/assets/icons/menu.svg'
+                        height={60}
+                        width={60}
+                        onClick={ () => setToggleDropdown(prev => !prev)} />
+                </div>
+            </div>
             </nav>
   )
 }
@@ -70,7 +92,7 @@ const Tab = ({ name, scrollToSection}) => {
     const activeTab = useContext(NavbarContext)
     return (
         <Link href={id}
-            className={`black_btn ${activeTab === id && 'active'}`}
+            className={`btn ${activeTab === id && 'active'}`}
             scroll={false}
             onClick={() => scrollToSection(id)}>
                       {name}
