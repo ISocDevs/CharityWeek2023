46b18aa6fecab5e3245585d1648ad33c15713ee2
Hamz <h4mz1221@gmail.com>
2023-09-11 13:37:32 +0100

    Add toggle menu on smaller screens

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -14,12 +14,12 @@ const Nav = () => {
     const [toggleDropdown, setToggleDropdown] = useState(false);
     const [clientWindowHeight, setClientWindowHeight] = useState("")
     const tabs = ['Home', 'About', 'Events', 'Contact', 'Sponsors', 'Socials']
-    
+
     const scrollToSection = (sectionId) => {
         const section = document.querySelector(sectionId);
         if (section) {
             setActiveTab(sectionId)
-            scrollIntoView(section, {behavior: 'smooth'})
+            scrollIntoView(section, { behavior: 'smooth' })
         }
     };
 
@@ -27,15 +27,26 @@ const Nav = () => {
         setClientWindowHeight(window.scrollY);
     };
 
-    useEffect(() => {
-        console.log(toggleDropdown)
-    },[toggleDropdown])
+    const handleClick = (e) => {
+        if (!e.target.closest('#menu-button')) {
+            // Click occurred outside the Image (menu button)
+            setToggleDropdown(false);
+        }
+    };
 
     useEffect(() => {
-    window.addEventListener("scroll", handleScroll);
-    return () => window.removeEventListener("scroll", handleScroll);
+        console.log(toggleDropdown)
+    }, [toggleDropdown])
+
+    useEffect(() => {
+        window.addEventListener("scroll", handleScroll);
+        document.body.addEventListener("click", handleClick);
+        return () => {
+            window.removeEventListener("scroll", handleScroll);
+            document.body.removeEventListener("click", handleClick);
+        };
     }, []);
-    
+
     useEffect(() => {
         const closestTab = tabs.reduce((prev, curr) => {
             const el = document.querySelector('#' + curr.toLowerCase())
@@ -43,73 +54,80 @@ const Nav = () => {
             return (Math.abs(el.getBoundingClientRect().top - 0) <= Math.abs(pre_el.getBoundingClientRect().top - 0) ? curr : prev)
         })
         setActiveTab('#' + closestTab.toLowerCase())
-    },[clientWindowHeight])
+    }, [clientWindowHeight])
 
     return (
         <nav className='flex-center flex-shrink-0 w-full py-2 fixed lg:px-10 px-2 top-0 backdrop-blur bg-white/20 shadow-lg'>
             {/* Logo */}
-          <Link href="/" className='flex gap-2 flex-center bg-red-100'>
-              <Image
-                  src="/assets/images/cw-logo-orange.ico"
-                  alt="Charity Week Logo"
-                  width={60}
-                  height={60}
-                  className="object-contain"
-              />
-              <p className='logo_text pr-5'>CW 2023</p>
-          </Link>
+            <Link href="/" className='flex gap-2 flex-center'>
+                <Image
+                    src="/assets/images/cw-logo-orange.ico"
+                    alt="Charity Week Logo"
+                    width={65}
+                    height={65}
+                    className="object-contain"
+                />
+                <p className='logo_text pr-5'>CW 2023</p>
+            </Link>
 
-          {/* Desktop Navigation */}
-          <div className='min-[800px]:flex hidden justify-center flex-grow bg-blue-100'>
+            {/* Desktop Navigation */}
+            <div className='min-[800px]:flex hidden justify-center flex-grow'>
                 <div className='flex lg:gap-3 gap-1'>
                     <NavbarContext.Provider value={activeTab}>
                         {tabs.map(tab =>
-                            <Tab name={tab} scrollToSection={scrollToSection} key={tab} />)}
+                            <Tab name={tab} scrollToSection={scrollToSection} key={tab} style='btn' />)}
                     </NavbarContext.Provider>
-                    <DonateButton style={'orange'}/>
-              </div>
+                    <DonateButton style={'orange'} />
+                </div>
             </div>
 
             {/* This exists just to make the formatting even and to have the tabs centered always
             I tried many other methods but this one gave me my desired result */}
-            <Link href="/" className='max-[799px]:hidden flex gap-2 flex-center flex-shrink bg-red-100 opacity-0 pointer-events-none'>
-              <Image
-                  src="/assets/images/cw-logo-orange.ico"
-                  alt="Charity Week Logo"
-                  width={60}
-                  height={60}
-                  className="object-contain"
-              />
-              <p className='logo_text pr-5'>CW 2023</p>
-          </Link>
-            
+            <Link href="/" className='max-[799px]:hidden flex gap-2 flex-center flex-shrink opacity-0 pointer-events-none'>
+                <Image
+                    src="/assets/images/cw-logo-orange.ico"
+                    alt="Charity Week Logo"
+                    width={65}
+                    height={65}
+                    className="object-contain"
+                />
+                <p className='logo_text pr-5'>CW 2023</p>
+            </Link>
+
             {/* Mobile navigation */}
-            <div className='min-[800px]:hidden flex-center w-full absolute'>
-                <DonateButton style={'pulse large'}/>
-                </div>
-            <div className='md:hidden'>
-                <div className=''>
+            <div className='min-[800px]:hidden flex-center w-4/5'>
+                <DonateButton style={'pulse large'} />
+            </div>
+            <div className='min-[800px]:hidden cursor-pointer'>
+                <div id="menu-button">
                     <Image
                         src='/assets/icons/menu.svg'
                         height={60}
                         width={60}
-                        onClick={ () => setToggleDropdown(prev => !prev)} />
+                        alt="Menu Button"
+                        onClick={() => setToggleDropdown(prev => !prev)} />
+                    {toggleDropdown && <div className='dropdown'>
+                        <NavbarContext.Provider value={activeTab}>
+                            {tabs.map(tab =>
+                                <Tab name={tab} scrollToSection={scrollToSection} key={tab} />)}
+                        </NavbarContext.Provider>
+                    </div>}
                 </div>
             </div>
-            </nav>
-  )
+        </nav>
+    )
 }
 
-const Tab = ({ name, scrollToSection}) => {
+const Tab = ({ name, scrollToSection, style }) => {
     const id = '#' + name.toLowerCase()
     const activeTab = useContext(NavbarContext)
     return (
         <Link href={id}
-            className={`btn ${activeTab === id && 'active'}`}
+            className={`${style ? style : ''} ${activeTab === id && 'active'}`}
             scroll={false}
             onClick={() => scrollToSection(id)}>
-                      {name}
-                  </Link>
+            {name}
+        </Link>
     )
 }
 
