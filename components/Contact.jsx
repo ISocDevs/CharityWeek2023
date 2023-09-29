d063ee7be9d96c10bd1bcb3019385382f04006ed
Hamz <h4mz1221@gmail.com>
2023-09-29 19:37:56 +0100

    Add responsive design to 'Contact' for smaller screen size

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -8,19 +8,22 @@ const rows = [
         key: "1",
         purpose: ["General"],
         heads: ["Safiyya Musa ", "Zain Ahmad"],
-        email: ["contact@imperialcharityweek.co.uk"],
+        email: [<a class="email"
+            href="mailto:contact@imperialcharityweek.co.uk?subject=Charity Week">contact@imperialcharityweek.co.uk</a>],
     },
     {
         key: "2",
         purpose: ["Auction Dinner"],
         heads: ["Esha Kamran", "Yusuf Maduddi"],
-        email: ["dinner@imperialcharityweek.co.uk"],
+        email: [<a class="email"
+            href="mailto:dinner@imperialcharityweek.co.uk?subject=Dinner">dinner@imperialcharityweek.co.uk</a>],
     },
     {
         key: "3",
         purpose: ["Sponsorship"],
         heads: ["Umar Mahmood", "Rayan Kamal"],
-        email: ["sponsorship@imperialcharityweek.co.uk"],
+        email: [<a class="email"
+            href="mailto:sponsorship@imperialcharityweek.co.uk?subject=Sponsorship">sponsorship@imperialcharityweek.co.uk</a>],
     }
 ];
 
@@ -39,21 +42,44 @@ const columns = [
     },
 ];
 
+const columns_sm = [
+    {
+        key: "email",
+        label: "Emails"
+    }
+]
+
 export default function Contact() {
     return (
-        <div >
-            <Table aria-label="Example table with dynamic content" >
-                <TableHeader columns={columns} >
-                    {(column) => <TableColumn key={column.key} className="text-lg">{column.label}</TableColumn>}
-                </TableHeader>
-                <TableBody items={rows}>
-                    {(item) => (
-                        <TableRow key={item.key}>
-                            {(columnKey) => <TableCell className="text-lg">{getKeyValue(item, columnKey).map(el => (<div>{el}</div>))}</TableCell>}
-                        </TableRow>
-                    )}
-                </TableBody>
-            </Table>
+        <div>
+            <div className="max-sm:hidden">
+                <Table aria-label="Example table with dynamic content" >
+                    <TableHeader columns={columns} >
+                        {(column) => <TableColumn key={column.key} className="text-lg">{column.label}</TableColumn>}
+                    </TableHeader>
+                    <TableBody items={rows}>
+                        {(item) => (
+                            <TableRow key={item.key}>
+                                {(columnKey) => <TableCell className="text-lg">{getKeyValue(item, columnKey).map(el => (<div>{el}</div>))}</TableCell>}
+                            </TableRow>
+                        )}
+                    </TableBody>
+                </Table>
+            </div>
+            <div className="sm:hidden">
+                <Table aria-label="Example table with dynamic content" >
+                    <TableHeader columns={columns_sm} >
+                        {(column) => <TableColumn key={column.key} className="text-lg text-center">{column.label}</TableColumn>}
+                    </TableHeader>
+                    <TableBody items={rows}>
+                        {(item) => (
+                            <TableRow key={item.key}>
+                                {(columnKey) => <TableCell className="text-lg text-center">{getKeyValue(item, columnKey).map(el => (<div>{el}</div>))}</TableCell>}
+                            </TableRow>
+                        )}
+                    </TableBody>
+                </Table>
+            </div>
         </div>
     );
 }
